ae8b8f1f1abbc2479e68d82e09c2440e6aaa424e
anhtudo97 <anhtudo97@gmail.com>
2021-10-17 23:29:47 +0700

    feat: add middleware

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            return res.status(401).json({ msg: "Unauthorized" });
+        }
+        const { userId } = jwt.verify(
+            req.headers.authorization,
+            process.env.JWT_SECRET
+        );
+
+        req.userId = userId;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ msg: "Unauthorized" });
+    }
+};
