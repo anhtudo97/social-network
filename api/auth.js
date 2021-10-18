32c752bc92a04f4a8b9c45fb5ad3cfac8d7c9659
anhtudo97 <anhtudo97@gmail.com>
2021-10-18 23:56:14 +0700

    feat: auth api update

--- api/auth.js
+++ api/auth.js
@@ -42,7 +42,6 @@ router.post("/", async(req, res) => {
             .json({ msg: "Password must be at least 6 characters" });
 
     try {
-
         // Check if user is registered
         const user = await User.findOne({ email: email.toLowerCase() }).selct(
             "+password"
@@ -64,9 +63,62 @@ router.post("/", async(req, res) => {
             if (err) throw err;
             res.status(200).send({ token });
         });
-
     } catch (error) {
         console.log(err);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+// @route:  PUT /api/auth
+// @desc:   Update user settings
+router.put("/", auth, upload.single("profilePic"), async(req, res) => {
+    try {
+        const { name, password } = req.body;
+
+        // Check if username is already taken
+        let user = await User.findOne({ username: username.toLowerCase() });
+        if (user && user._id.toString() !== req.userId) {
+            return res.status(400).json({ msg: "Username is already taken" });
+        }
+
+        const updateUser = {};
+        if (name) updateUser.name = name;
+        if (username) updateUser.username = username;
+        if (req.file && req.file.path) updateUser.profilePicUrl = req.file.path;
+
+        user = await User.findByIdAndUpdate(req.userId, updateUser, { new: true });
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+// @route:  PUT /api/auth/password
+// @desc:   Update password
+router.put("/", auth, async(req, res) => {
+    try {
+        const { currentPassword, newPassword } = req.body;
+
+        const user = await User.findById(req.userId).select("+password");
+        if (!user) return res.status(404).json({ msg: "User not found" });
+
+        // Check if current password matches
+        const isMatch = await bcrypt.compare(currentPassword, newPassword);
+        if (!isMatch) return res.status(401).json({ msg: "Incorrect password" });
+
+        if (newPassword.length < 6)
+            return res
+                .status(400)
+                .json({ msg: "Password must be atleast 6 characters long" });
+
+        user.password = await bcrypt.hash(newPassword, 10);
+        await user.save();
+
+        res.status(200).json({ msg: "Password updated" });
+    } catch (error) {
+        console.log(error);
         res.status(500).json({ msg: "Server error" });
     }
 });
