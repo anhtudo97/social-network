c2c141aa8b1347d29725dbeced2ed7edb8eea62d
Zhao Yang <anhtudo97@gmail.com>
2021-11-06 19:12:48 +0700

    feat(utils): update

---
+++ utils/messageNotification.js
@@ -0,0 +1,14 @@
+export const messageNotification = (senderName) => {
+    const sound = new Audio("/new_message.mp3");
+    sound && sound.play();
+
+    if (senderName) {
+        document.title = `New message from ${senderName}`;
+
+        if (document.visibilityState === "visible") {
+            setTimeout(() => {
+                document.title = "Your message | Driwwle";
+            }, 3000);
+        }
+    }
+};
