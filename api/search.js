37e392ca2a1802b59ab9907df6fce9e79ef4dc73
Zhao Yang <anhtudo97@gmail.com>
2021-11-12 02:19:02 +0700

    feat(api): search

---
+++ api/search.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/User.model");
+const Post = require("../models/Post.model");
+
+const auth = require("../middleware/auth.middleware");
+
+// @route:  GET /api/search/:searchText
+// @desc:   Get users and posts related to search text
+router.get("/:searchText", async(req, res) => {
+    const { searchText } = req.params;
+    if (searchText.trim().length === 0)
+        return res.status(400).json({ msg: "Search text too short" });
+
+    try {
+        const users = await User.find({
+            $or: [
+                { name: { $regex: searchText, $options: "i" } },
+                { username: { $regex: searchText, $options: "i" } },
+            ],
+            isVerified: true,
+        }).limit(3);
+
+        const posts = await Post.find({
+                title: { $regex: searchText, $options: "i" },
+            })
+            .populate("user")
+            .limit(3);
+
+        res.status(200).json({ users, posts });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+// @route:  GET /api/search/users/:searchText
+// @desc:   Get users related to search text
+router.get("/users/:seacheText", auth, async(req, res) => {
+    const { seacheText } = req.params;
+});
