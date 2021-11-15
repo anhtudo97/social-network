13399803c6cf59284a8e82029fa2d28d5532fda9
Zhao Yang <anhtudo97@gmail.com>
2021-11-15 23:32:53 +0700

    feat(api): comments update

--- api/comments.js
+++ api/comments.js
@@ -170,3 +170,141 @@ router.post("/:postId/:commentId", auth, async(req, res) => {
         res.status(500).json({ msg: "Server error" });
     }
 });
+
+// @route   DELETE /api/comments/:postId/:commentId/:replyId
+// @desc    Delete a reply to comment
+router.delete("/:postId/:commentId/:replyId", auth, async(req, res) => {
+    try {
+        let post = await Comment.findOne({ post: req.params.postId });
+        if (!post) return res.status(404).json({ msg: "Post not found" });
+
+        const parentComment = post.comments.find(
+            (comment) => comment._id === req.params.commentId
+        );
+        if (!parentComment)
+            return res.status(404).json({ msg: "Comment not found" });
+
+        const reply = parentComment.replies.find(
+            (reply) => reply._id === req.params.replyId
+        );
+        if (!reply) return res.status(404).json({ msg: "Reply not found" });
+
+        const user = await User.findById(req.userId);
+
+        if (reply.user.toString() === req.userId || user.role === "root") {
+            const index = parentComment.replies.findIndex(
+                (reply) => reply._id === req.params.replyId
+            );
+            parentComment.replies.splice(index, 1);
+            post = await post.save();
+
+            post = await Comment.populate(post, "comments.user");
+            post = await Comment.populate(post, "comments.replies.user");
+
+            if (parentComment.user._id.toString() !== req.userId) {
+                await removeReplyNotification(
+                    parentComment.user._id.toString(),
+                    req.userId,
+                    req.params.postId,
+                    reply._id
+                );
+            }
+
+            res.status(200).json(post.comments);
+        } else {
+            res
+                .status(401)
+                .json({ msg: "You are not authorized to delete this comment" });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+// @route   PUT /api/comments/like/:postId/:commentId
+// @desc    Like or unlike a comment
+router.put("/like/:postId/:commentId", auth, async(req, res) => {
+    try {
+        let post = await Comment.findOne({ post: req.params.postId });
+        if (!post) return res.status(404).json({ msg: "Post not found" });
+
+        let comment = post.comments.find(
+            (comment) => comment._id === req.params.commentId
+        );
+        if (!comment) return res.status(404).json({ msg: "Comment not found" });
+
+        const isLiked =
+            comment.likes.filter((like) => like.user.toString() === req.userId)
+            .length > 0;
+
+        if (isLiked) {
+            // Unlike the comment if already liked
+            const index = comment.likes.findIndex(
+                (like) => like.user.toString() === req.userId
+            );
+            comment.likes.splice(index, 1);
+        } else {
+            // Like the comment
+            comment.likes.unshift({ user: req.userId });
+        }
+
+        // Save post and send response
+        post = await post.save();
+
+        post = await Comment.populate(post, "comments.user");
+        post = await Comment.populate(post, "comments.replies.user");
+
+        res.status(200).json(post.comments);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+// @route   PUT /api/comments/like/:postId/:commentId/:replyId
+// @desc    Like or unlike a reply
+router.put("/like/:postId/:commentId/:replyId", auth, async(req, res) => {
+    try {
+        let post = await Comment.findOne({ post: req.params.postId });
+        if (!post) return res.status(404).json({ msg: "Post not found" });
+
+        let comment = post.comments.find(
+            (comment) => comment._id === req.params.commentId
+        );
+        if (!comment) return res.status(404).json({ msg: "Comment not found" });
+
+        let reply = comment.replies.find(
+            (reply) => reply._id === req.params.replyId
+        );
+        if (!reply) return res.status(404).json({ msg: "Reply not found" });
+
+        const isLiked =
+            reply.likes.filter((like) => like.user.toString() === req.userId).length >
+            0;
+
+        if (isLiked) {
+            // Unlike the reply if already liked
+            const index = reply.likes.findIndex(
+                (like) => like.user.toString() === req.userId
+            );
+            reply.likes.splice(index, 1);
+        } else {
+            // Like the comment
+            reply.likes.unshift({ user: req.userId });
+        }
+
+        // Save post and send response
+        post = await post.save();
+
+        post = await Comment.populate(post, "comments.user");
+        post = await Comment.populate(post, "comments.replies.user");
+
+        res.status(200).json(post.comments);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ msg: "Server error" });
+    }
+});
+
+module.exports = router;
