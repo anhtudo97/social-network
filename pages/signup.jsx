b98632063d0098b10fa6254bed0be78e1147ec56
Zhao Yang <anhtudo97@gmail.com>
2021-12-20 23:02:12 +0700

    feat: update

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -137,9 +137,8 @@ const Signup = () => {
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   {usernameLoading || username === '' ? (
                     <DotsCircleHorizontalIcon
-                      className={`h-5 w-5 text-gray-400 ${
-                        usernameLoading && 'animate-spin'
-                      }`}
+                      className={`h-5 w-5 text-gray-400 ${usernameLoading && 'animate-spin'
+                        }`}
                       aria-hidden="true"
                     />
                   ) : username !== '' && usernameAvailable ? (
@@ -152,13 +151,13 @@ const Signup = () => {
                   type="text"
                   name="username"
                   id="username"
-                  className={`focus:ring-pink-500 focus:border-pink-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md ${
-                    username !== '' && !usernameAvailable ? 'bg-red-100' : ''
-                  }`}
+                  className={`focus:ring-pink-500 focus:border-pink-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md ${username !== '' && !usernameAvailable ? 'bg-red-100' : ''
+                    }`}
                   placeholder="something_legendary"
                   value={username}
                   onChange={(e) => {
                     setUsername(e.target.value);
+                    console.log(usernameRegex.test(e.target.value));
                     if (usernameRegex.test(e.target.value)) {
                       setUsernameAvailable(true);
                     } else {
--- utils/baseURL.js
+++ utils/baseURL.js
@@ -1,2 +1,3 @@
-export const baseURL =
-    process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+export default baseURL;
